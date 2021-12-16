bb54c3e23c332be9c507a0f0df5e87d9cfec4aee
ecsbeats <adamblumenfeldprofessional@gmail.com>
2021-12-15 19:21:19 -0800

    (backend) Fix typo in settings.py

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
   return <div>Next.JS Boilerplate</div>
 }
+
 export default Home
