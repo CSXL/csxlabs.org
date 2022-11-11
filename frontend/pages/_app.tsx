ee97f2ac64d825b25e2d562755ba5b6df7d42794
ecsbeats <adamb@csxlabs.org>
2022-11-11 14:14:30 -0800

    add github primer

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { BaseStyles, ThemeProvider, Box } from '@primer/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <BaseStyles>
+        <Box fontFamily={"normal"}>
+          <Component {...pageProps} />
+        </Box>
+      </BaseStyles>
+    </ThemeProvider>
+  )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Box, Heading, Header, StyledOcticon, Avatar } from '@primer/react'
+import { RocketIcon } from '@primer/octicons-react'
 
 export default function Home() {
   return (
-    <h1>CSX Labs. Website under construction</h1>
+    <>
+      <Header>
+        <Header.Item>
+         <Header.Link href="#" fontSize={2}>
+            <StyledOcticon icon={RocketIcon} size={32} sx={{mr: 2}}></StyledOcticon>
+            <span>CSX Labs</span>
+          </Header.Link>
+        </Header.Item>
+        <Header.Item full>
+
+        </Header.Item>
+        <Header.Item mr={0}>
+          <Avatar src="https://github.com/octocat.png" size={20} square alt="@octocat" />
+        </Header.Item>
+      </Header>
+      <Box m={4}>
+        <Heading>CSX Labs. Website under construction</Heading>
+      </Box>
+    </>
   )
 }
